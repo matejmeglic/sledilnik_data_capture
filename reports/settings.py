3e4bb2bc25242d9fc50a460a4d3f17419024d6f5
matejmeglic <matej@matejmeglic.com>
2020-08-14 12:51:12 +0200

    Git_update

--- reports/settings.py
+++ reports/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "!e2cz*+x-di5@r56vggpm8&zmmso&2xnoc$8##+^8y8endyqld"
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
