9fbb92600308c7c2f501170c39a87e7f23a98eb6
matejmeglic <matej@matejmeglic.com>
2020-08-18 09:40:14 +0200

    settings changes1

--- reports/settings.py
+++ reports/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://sledilnik-data-capture.herokuapp.com/"]
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
